7deadcfe644a2e8f9aea0e2714a83b819072d45f
David Gutiérrez Tovar <davidgutierreztovar@MacBook-Pro-de-David.local>
2021-09-22 19:59:38 -0500

    add crud routes

---
+++ server/routes/categories.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+
+// CRUD
+
+// Get all products
+router.get('/',  () => {
+
+})
+
+// Create a new product
+router.post('/',  () => {
+
+})
+
+
+// Update a product by id
+router.put('/:id',  () => {
+
+})
+
+
+// Delete a product by id
+router.get('/:id',  () => {
+
+})
+
+
+
+module.exports = router;
--- server/routes/providers.js
+++ server/routes/providers.js
@@ -2,11 +2,36 @@ const express = require('express');
 const router = express.Router();
 
 
+// CRUD
 
-
+// Get all products
 router.get('/',  () => {
 
 })
+
+
+// Create a new product
+router.post('/',  () => {
+
+})
+
+
+// Update a product by id
+router.put('/:id',  () => {
+
+})
+
+
+
+// Delete a product by id
+router.get('/:id',  () => {
+
+})
+
+
+
+
+
 
 
 
